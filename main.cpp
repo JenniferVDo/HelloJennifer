4bf777319adbe7e9c546436a2aada513b07662cd
JenniferVDo <jennifervelazquez54@gmail.com>
2022-06-22 13:21:49 -0500

    Added function sum

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+int sum(int a, int b){
+    return a+b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    sum(2,3);
     return 0;
 }
