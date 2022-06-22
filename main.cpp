71ea0631327fbc72ca39af92508c810b16b4c8dd
JenniferVDo <jennifervelazquez54@gmail.com>
2022-06-22 13:36:04 -0500

    Added function float summ

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 int sum(int a, int b){
     return a+b;
 }
+float summ(int a, int b){
+    return a+b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     sum(2,3);
+    summ(3,5);
     return 0;
 }
